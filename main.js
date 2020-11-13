1bdc1aafba6306f0e0b1b36563218c2d1248daa3
Paula Nerenberg <paula1@ymail.com>
2020-11-13 14:31:28 +0000

    hello

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/hello';
+
+           //https://api.dictionaryapi.dev/api/v2/entries/en/
+
 const targetEl = document.getElementById('targetWord');
 const findBtn = document.getElementById('findBtn');
 
 async function dictionary() {
     const response = await fetch(url);
     const data = await response.json();
-    findBtn.addEventListener('click', function(ev) {
+    // findBtn.addEventListener('click', function(ev) {
         
-    });
+    // });
 
 
     console.log(data);
+    return data;
 }
+
+const button = document.createElement('button');
+button.type = 'button';
+button.innerText = 'asClick';
+document.body.appendChild (button);
+
+function addButton()
