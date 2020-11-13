f837f538bc642ef72e29ee94551f9d1c14b30ad3
Daniel Bergmann <dba222@outlook.com>
2020-11-13 15:13:53 +0000

    fiished the session

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/hello';
+
 
            //https://api.dictionaryapi.dev/api/v2/entries/en/
 
-const targetEl = document.getElementById('targetWord');
+
 const findBtn = document.getElementById('findBtn');
 
 async function dictionary() {
+    
+    let word = document.getElementById('targetWord').value;
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    
+    
+    const defText = document.getElementById('definition');
+
+    defText.innerHTML = word;
+
+    
+
     const response = await fetch(url);
     const data = await response.json();
-    // findBtn.addEventListener('click', function(ev) {
-        
-    // });
-
-
-    console.log(data);
-    return data;
+    console.log(data[0].meanings[0].definitions[0].definition);
+    try{
+        defText.innerHTML = data[0].meanings[0].definitions[0].definition;
+    }
+    catch{
+        console.log('error..')
+    }
 }
 
-const button = document.createElement('button');
-button.type = 'button';
-button.innerText = 'asClick';
-document.body.appendChild (button);
 
-function addButton()
+ findBtn.addEventListener('click', dictionary);
+
